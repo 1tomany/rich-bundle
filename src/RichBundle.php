6c24d6ac0afe64863a97aae7c1e6431c307bebe7
Vic Cherubini <vcherubini@gmail.com>
2025-01-30 16:00:52 -0600

    wip

--- src/RichBundle.php
+++ src/RichBundle.php
@@ -2,14 +2,19 @@
 
 namespace OneToMany\RichBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class RichBundle extends AbstractBundle
 {
 
+    /**
+     * @param array<string, mixed> $config
+     */
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.xml');
     }
-    
+
 }
