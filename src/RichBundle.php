5ef736ee9e5789e8b39962061e72c117e000acab
Vic Cherubini <vcherubini@gmail.com>
2025-01-30 15:55:40 -0600

    wip

--- src/RichBundle.php
+++ src/RichBundle.php
@@ -6,4 +6,10 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class RichBundle extends AbstractBundle
 {
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.xml');
+    }
+    
 }
