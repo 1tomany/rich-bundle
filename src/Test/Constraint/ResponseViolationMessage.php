a9ac74cb583849f1608ada899a11eb1c5cb9f0bc
Vic Cherubini <vcherubini@gmail.com>
2025-04-10 15:47:07 -0500

    adds a new constraint to easily test violation messages

---
+++ src/Test/Constraint/ResponseViolationMessage.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace OneToMany\RichBundle\Test\Constraint;
+
+use PHPUnit\Framework\Constraint\Constraint;
+use Symfony\Component\HttpFoundation\Response;
+
+use function get_debug_type;
+use function is_array;
+use function json_decode;
+use function sprintf;
+
+final class ResponseViolationMessage extends Constraint
+{
+    public function __construct(
+        private readonly string $property,
+        private readonly string $message,
+    ) {
+    }
+
+    public function toString(): string
+    {
+        return sprintf('the property "%s" has a violation "%s"', $this->property, $this->message);
+    }
+
+    /**
+     * @param Response $response
+     */
+    protected function matches(mixed $response): bool
+    {
+        if (!$response instanceof Response) {
+            throw new \InvalidArgumentException(sprintf('Expected argument of type "%s", "%s" given.', Response::class, get_debug_type($response)));
+        }
+
+        $content = $response->getContent();
+
+        if (!$content) {
+            return false;
+        }
+
+        $json = json_decode($content, true);
+
+        if (!is_array($json)) {
+            return false;
+        }
+
+        $violations = $json['violations'] ?? null;
+
+        if (!is_array($violations)) {
+            return false;
+        }
+
+        $hasPropertyAndMessage = false;
+
+        foreach ($violations as $v) {
+            if (!is_array($v)) {
+                continue;
+            }
+
+            if ($this->property === $v['property']) {
+                if ($this->message === $v['message']) {
+                    $hasPropertyAndMessage = true;
+                }
+            }
+        }
+
+        return $hasPropertyAndMessage;
+    }
+
+    /**
+     * @param Response $response
+     */
+    protected function failureDescription($response): string
+    {
+        return $this->toString();
+    }
+}
