67127ec1f3b19b38494774b6e2a31ffe076c3d18
Vic Cherubini <vcherubini@gmail.com>
2025-04-11 15:49:11 -0500

    wip

--- src/RichBundle.php
+++ src/RichBundle.php
@@ -2,7 +2,6 @@
 
 namespace OneToMany\RichBundle;
 
-use OneToMany\RichBundle\Contract\HandlerInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -15,17 +14,5 @@ class RichBundle extends AbstractBundle
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.xml');
-
-        // $container->instanceof(HandlerInterface::class)->tag('messenger.message_handler');
-
-        $builder
-                    ->registerForAutoconfiguration(HandlerInterface::class)
-                    ->addTag('messenger.message_handler', ['method' => 'handle'])
-                ;
-
-        // $def = $builder->findDefinition(HandlerInterface::class);
-        // var_dump(gettype($def));
-        // var_dump($builder->has(HandlerInterface::class));
-        // $builder->registerAttributeForAutoconfiguration();
     }
 }
