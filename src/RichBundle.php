a4f9dd90b7fbc00877a3eef873d8d643a8e80f69
Vic Cherubini <vcherubini@gmail.com>
2025-04-14 15:49:47 -0500

    sets the priority to ensure it is executed first

--- src/RichBundle.php
+++ src/RichBundle.php
@@ -13,10 +13,12 @@ class RichBundle extends AbstractBundle
     {
         parent::build($container);
 
-        // Add the messenger.message_handler tag to all classes
-        // that implement the HandlerInterface interface, and
-        // ensure their handles() method uses the correct command
-        $container->addCompilerPass(new RegisterModulesPass());
+        // This pass adds the 'messenger.message_handler' tag to all classes
+        // that implement the HandlerInterface interface, and sets the command
+        // class each handler's handle() method should use. The priority is set
+        // to a value higher than the priority of the compiler pass provided by
+        // the Symfony Messenger component to ensure this pass is processed first.
+        $container->addCompilerPass(pass: new RegisterModulesPass(), priority: 1);
     }
 
     /**
