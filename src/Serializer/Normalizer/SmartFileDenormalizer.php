7c28f9cf6b395f99484ee75db74ab79a39fb3c19
Vic Cherubini <vcherubini@gmail.com>
2025-04-22 13:18:06 -0500

    updates to version 4.0.0 of the 1tomany/data-uri package to use SmartFile instead of LocalFile

--- src/Serializer/Normalizer/SmartFileDenormalizer.php
+++ src/Serializer/Normalizer/SmartFileDenormalizer.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
 use function is_a;
-use function is_bool;
 use function is_string;
 use function OneToMany\DataUri\parse_data;
 
@@ -19,25 +18,7 @@ final readonly class SmartFileDenormalizer implements DenormalizerInterface
      */
     public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): SmartFile
     {
-        $tempDir = $context['temp_dir'] ?? null;
-
-        if (!is_string($tempDir)) {
-            $tempDir = null;
-        }
-
-        $hashAlgorithm = $context['hash_algorithm'] ?? null;
-
-        if (!is_string($hashAlgorithm)) {
-            $hashAlgorithm = 'sha256';
-        }
-
-        $selfDestruct = $context['self_destruct'] ?? null;
-
-        if (!is_bool($selfDestruct)) {
-            $selfDestruct = true;
-        }
-
-        return parse_data($data, $tempDir, $hashAlgorithm, $selfDestruct);
+        return parse_data($data);
     }
 
     /**
