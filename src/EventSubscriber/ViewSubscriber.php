afd1db8eedc2520b4c6b9c893dd98b92f6d49383
Vic Cherubini <vcherubini@gmail.com>
2025-07-19 22:30:10 -0500

    wip

--- src/EventSubscriber/ViewSubscriber.php
+++ src/EventSubscriber/ViewSubscriber.php
@@ -4,10 +4,12 @@ namespace OneToMany\RichBundle\EventSubscriber;
 
 use OneToMany\RichBundle\Contract\Action\ResultInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Serializer\SerializerInterface;
+
+use function strtolower;
 
 final readonly class ViewSubscriber implements EventSubscriberInterface
 {
@@ -26,21 +28,24 @@ final readonly class ViewSubscriber implements EventSubscriberInterface
 
     public function renderResultResponse(ViewEvent $event): void
     {
-        $result = $event->getControllerResult();
+        if (($result = $event->getControllerResult()) instanceof ResultInterface) {
+            // Determine the response format by inspecting the "Accept" header
+            $format = $event->getRequest()->getPreferredFormat('json') ?? 'json';
 
-        if (!$result instanceof ResultInterface) {
-            return;
+            // Attempt to serialize the result value
+            $responseContent = $this->serializer->serialize(
+                $result(), $format, $result->getContext()
+            );
+
+            $contentType = $event->getRequest()->getMimeType(...[
+                'format' => strtolower($format),
+            ]);
+
+            $response = new Response($responseContent, $result->getStatus(), $result->getHeaders() + [
+                'Content-Type' => strtolower($contentType ?? 'application/json'),
+            ]);
+
+            $event->setResponse($response);
         }
-
-        $data = $this->render($result, $event->getRequest()->getPreferredFormat('json'));
-
-        /*
-        $event->setResponse(new JsonResponse($data, $result->getStatus(), $result->getHeaders(), true));
-        */
-    }
-
-    private function render(ResultInterface $result, ?string $format): string
-    {
-        return $this->serializer->serialize($result(), $format ?? 'json', $result->getContext());
     }
 }
