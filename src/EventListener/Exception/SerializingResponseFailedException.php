c12a15eedeac8cc2f4d53f1b92a11122298276b1
Vic Cherubini <vcherubini@gmail.com>
2025-07-21 18:31:38 -0500

    wip

---
+++ src/EventListener/Exception/SerializingResponseFailedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OneToMany\RichBundle\EventListener\Exception;
+
+use OneToMany\RichBundle\Exception\RuntimeException;
+
+final class SerializingResponseFailedException extends RuntimeException
+{
+    public function __construct(mixed $data, ?\Throwable $previous = null)
+    {
+        parent::__construct(\sprintf('Serializing the response failed because data of type "%s" could not be encoded.', \get_debug_type($data)), 500, $previous);
+    }
+}
