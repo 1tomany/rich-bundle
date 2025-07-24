20abea491386aa81eb87c107d2748503a9bb9485
Vic Cherubini <vcherubini@gmail.com>
2025-07-24 12:28:23 -0500

    wip

--- src/Exception/InvalidArgumentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OneToMany\RichBundle\Exception;
-
-use OneToMany\RichBundle\Contract\Exception\ExceptionInterface;
-
-class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
-{
-}
