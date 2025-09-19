9c517313f29e644c7118a8d380d7534d1aa2537b
Vic Cherubini <vcherubini@gmail.com>
2025-09-19 17:49:13 -0500

    wip

--- src/Security/FailureHandlerTrait.php
+++ src/Security/FailureHandlerTrait.php
@@ -10,14 +10,14 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 trait FailureHandlerTrait // @phpstan-ignore trait.unused
 {
     /**
-         * This method throws the original AuthenticationException to
-         * allow the user to invoke their own handler. Without it, the
-         * default Symfony exception handler is invoked and formats the
-         * response. By throwing the exception, we ensure that all API
-         * responses are consistently formatted.
-         *
-         * @throws HttpExceptionInterface
-         */
+     * This method throws the original AuthenticationException to
+     * allow the user to invoke their own handler. Without it, the
+     * default Symfony exception handler is invoked and formats the
+     * response. By throwing the exception, we ensure that all API
+     * responses are consistently formatted.
+     *
+     * @throws HttpExceptionInterface
+     */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): never
     {
         throw HttpException::fromStatusCode(401, $exception->getMessage(), $exception);
