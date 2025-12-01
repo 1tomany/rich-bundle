c4e900ed07b9ed107bc78f189e4c9bb2cd6a5b2d
Vic Cherubini <vcherubini@gmail.com>
2025-12-01 16:19:41 -0600

    wip

--- tests/Validator/UninitializedPropertiesTest.php
+++ tests/Validator/UninitializedPropertiesTest.php
@@ -2,16 +2,9 @@
 
 namespace OneToMany\RichBundle\Tests\Validator;
 
-use OneToMany\RichBundle\Contract\Action\CommandInterface;
-use OneToMany\RichBundle\Contract\Action\InputInterface;
 use OneToMany\RichBundle\Validator\UninitializedProperties;
-use OneToMany\RichBundle\Validator\UninitializedPropertiesValidator;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\ConstraintValidatorInterface;
-use Symfony\Component\Validator\Exception\UnexpectedTypeException;
-use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
 
 #[Group('UnitTests')]
 #[Group('ValidatorTests')]
