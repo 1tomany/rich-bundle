0e5d359b7605c9103197f8ca9edc2ba65b4d31fd
Vic Cherubini <vcherubini@gmail.com>
2026-04-11 00:57:40 -0500

    wip

--- src/Attribute/HasErrorType.php
+++ src/Attribute/HasErrorType.php
@@ -7,7 +7,8 @@ use OneToMany\RichBundle\Contract\Enum\ErrorType;
 #[\Attribute(\Attribute::TARGET_CLASS)]
 final readonly class HasErrorType
 {
-    public function __construct(public ErrorType $type = ErrorType::Logic)
-    {
+    public function __construct(
+        public ErrorType $type = ErrorType::Logic,
+    ) {
     }
 }
